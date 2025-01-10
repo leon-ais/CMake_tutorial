59ea9ab4591456eb2b98ab03fa1255022841bbed
leon-ais <leon_ais@qq.com>
2025-01-10 23:47:21 +0800

    add CMakeLists.txt vs building scripts.

--- 02-simple_demo/build.sh
+++ 02-simple_demo/build.sh
@@ -6,21 +6,36 @@
 #########################################################################
 #!/bin/bash
 
+# 获取脚本所在目录的绝对路径
+SCRIPT_DIR=$(dirname $(realpath "$0"))
+
 # 如果存在bin目录，则删除
-if [ -d "bin" ]; then
-    rm -rf bin
+if [ -d "$SCRIPT_DIR/bin" ]; then
+    rm -rf "$SCRIPT_DIR/bin"
 fi
 # 如果存在build目录，则删除
-if [ -d "build" ]; then
-    rm -rf build
+if [ -d "$SCRIPT_DIR/build" ]; then
+    rm -rf "$SCRIPT_DIR/build"
 fi
 # 创建build目录
-mkdir build
+mkdir "$SCRIPT_DIR/build"
 # 进入build目录
-cd build
-# 执行clang++编译
-clang++ -std=c++17 ../src/*.cpp -o my_calculator
+cd "$SCRIPT_DIR/build"
+
+# 定义可执行程序名
+EXECUTABLE_NAME="my_calculator"
+
+# 判断操作系统（win下执行有问题）
+OS=$(uname -s)
+if [ "$OS" = "Darwin" ]; then
+    # macOS系统
+    clang++ -std=c++17 "$SCRIPT_DIR/src/*.cpp" -o "$EXECUTABLE_NAME"
+else
+    # Windows, linux, others
+    g++ -std=c++17 "$SCRIPT_DIR/src/*.cpp" -o "$EXECUTABLE_NAME"
+fi
+
 # 创建bin目录拷贝可执行文件
-cd .. && mkdir bin && cp build/my_calculator bin && cd bin
+mkdir "$SCRIPT_DIR/bin" && cp "$SCRIPT_DIR/build/$EXECUTABLE_NAME"* "$SCRIPT_DIR/bin" && cd "$SCRIPT_DIR/bin"
 # 执行可执行文件
-./my_calculator
+./$EXECUTABLE_NAME

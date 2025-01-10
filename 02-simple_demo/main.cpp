7f27c854e43430ca27a5e1b0eded116c0db5eae3
ArrisOrz <leon_ais@qq.com>
2025-01-10 09:19:45 +0800

    Update main.cpp
    Change "math.h" -> "calc.h"

--- 02-simple_demo/main.cpp
+++ 02-simple_demo/main.cpp
@@ -1,18 +1,13 @@
 #include <ctime>
 #include <iostream>
-#include "math.h"
+#include "calc.h"
 
 using namespace std;
 
 int main()
 {
-    int a = 0;
-    int b = 0;
-    // 调用 srand 函数设置随机数种子
-    srand(static_cast<unsigned int>(time(nullptr)));
-    // 随机生成两个整数
-    a = rand() % 100;
-    b = rand() % 100;
+    int a = 27;
+    int b = 4;
 
     // 调用四个源文件执行四则运算
     cout << a << " + " << b << " = " << myadd(a, b) << endl;

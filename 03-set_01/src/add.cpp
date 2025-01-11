c138c57437c860941a6365521390fc4267820bcd
leon-ais <leon_ais@qq.com>
2025-01-11 20:43:35 +0800

    Auto add source code file.

---
+++ 03-set_01/src/add.cpp
@@ -0,0 +1,6 @@
+#include "calc.h"
+
+// 实现加法
+int myadd(int a, int b) {
+    return a + b;
+}
---
+++ 03-set_01/src/calc.h
@@ -0,0 +1,10 @@
+#ifndef CALC_H
+#define CALC_H
+
+// 函数声明
+int myadd(int a, int b);
+int mysub(int a, int b);
+int mymul(int a, int b);
+double mydiv(int a, int b);
+
+#endif // CALC_H
